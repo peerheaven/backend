c83d3a8b5371b04f340efa0512fe58633986f039
Tammy <tanmoy22chowdhury@gmail.com>
2020-11-22 10:44:49 +0530

    added sevlet

---
+++ bin/README.md
@@ -0,0 +1,2 @@
+# backend
+sample check
--- src/main/java/com/example/peerheavenbackend/PeerheavenbackendApplication.java
+++ src/main/java/com/example/peerheavenbackend/PeerheavenbackendApplication.java
@@ -2,12 +2,20 @@ package com.example.peerheavenbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PeerheavenbackendApplication {
-
+public class PeerheavenbackendApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(PeerheavenbackendApplication.class, args);
 	}
+	
+	 @Override
+	    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	        return application.sources(PeerheavenbackendApplication.class);
+	    }
+
+	
 
 }
